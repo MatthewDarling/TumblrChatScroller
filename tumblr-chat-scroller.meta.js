37499d06caa9fcb4c4efb78b2db7234a7b26233d
Matthew Darling <matthewjdarling@gmail.com>
2018-08-29 00:17:51 -0400

    Create GreaseMonkey metadata file for improved update support
    What should happen is that the original install URL is taken, and the
    "user.js" is switched with "meta.js", and then it parses the
    `@version` part. If that number is increased, then redownload from the
    original install URL.

---
+++ tumblr-chat-scroller.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name Tumblr Chat Scroller
+// @author Matthew Darling
+// @namespace Violentmonkey Scripts
+// @match *://www.tumblr.com/blog/*/activity
+// @grant none
+// @require https://raw.githubusercontent.com/moment/moment/2.22.2/moment.js
+// @require https://raw.githubusercontent.com/jsmreese/moment-duration-format/2.2.2/lib/moment-duration-format.js
+// ==/UserScript==
